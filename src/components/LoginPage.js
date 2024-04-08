66d39871167ec587472d9bf9ab1dc28907dfca20
Tehreem <tehreem.sajid@ucalgary.ca>
2024-04-08 12:24:58 -0600

    .

---
+++ src/components/LoginPage.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import HomeMainSection from './HomeMainSection';
+import Footer from './Footer';
+import reviewsData from '../data/reviews'; 
